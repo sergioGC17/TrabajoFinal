c6c025e4b82e160449bd898a3fd28fe3f67535b5
Sergio <sgomecuq@mmyuax.com>
2023-06-05 00:36:16 +0200

    terminada la pagina de modelo S.html

--- js/script.js
+++ js/script.js
@@ -31,3 +31,75 @@ function myFunction() {
       x.style.display = "block";
   }
 }
+
+
+//Carrusel
+window.onload = function () {
+  const radioRojo = document.getElementById("rojo");
+  const radioNegro = document.getElementById("negro");
+  const radioBlanco = document.getElementById("blanco");
+
+  radioRojo.addEventListener("change", () => {
+    if (radioRojo.checked) {
+      imagenes[0] = "img/rojo/compositor1.png";
+      imagenes[1] = "img/rojo/compositor2.png";
+      imagenes[2] = "img/rojo/compositor3.png";
+      imagenes[3] = "img/rojo/compositor4.png";
+      fotoActual = 0;
+      cargarImagen();
+    }
+  });
+  radioNegro.addEventListener("change", () => {
+    if (radioNegro.checked) {
+      imagenes[0] = "img/negro/compositor1.png";
+      imagenes[1] = "img/negro/compositor2.png";
+      imagenes[2] = "img/negro/compositor3.png";
+      imagenes[3] = "img/negro/compositor4.png";
+      fotoActual = 0;
+      cargarImagen();
+    }
+  });
+  radioBlanco.addEventListener("change", () => {
+    if (radioBlanco.checked) {
+      imagenes[0] = "img/blanco/compositor1.png";
+      imagenes[1] = "img/blanco/compositor2.png";
+      imagenes[2] = "img/blanco/compositor3.png";
+      imagenes[3] = "img/blanco/compositor4.png";
+      fotoActual = 0;
+      cargarImagen();
+    }
+  });
+
+
+  const tiempo = 1000;
+  let fotoActual = 0;
+  let $anterior = document.querySelector('#anterior');
+  let $siguiente = document.querySelector('#siguiente');
+  let $imagen = document.querySelector('#imagen');
+  let intervalo;
+
+  function siguienteFoto() {
+      if(fotoActual >= imagenes.length - 1) {
+          fotoActual = 0;
+      } else {
+          fotoActual++;
+      }
+      cargarImagen();
+  }
+  function AnteriorFoto() {
+      if(fotoActual <= 0) {
+          fotoActual = imagenes.length - 1;
+      } else {
+          fotoActual--;
+      }
+      cargarImagen();
+  }
+  function cargarImagen () {
+      $imagen.style.backgroundImage = `url(${imagenes[fotoActual]})`;
+  }
+  $siguiente.addEventListener('click', siguienteFoto);
+  $anterior.addEventListener('click', AnteriorFoto);
+  cargarImagen();
+} 
+
+
